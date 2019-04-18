780528c02ef6b751d20cfd57395087b3bb0bbd99
BearlyCorporeal <48078310+BearlyCorporeal@users.noreply.github.com>
2019-04-18 15:32:00 +1000

    finished all tasks
    they are all there but most of them are named poorly sorry

---
+++ repetition task ex1p4/repetition task ex1p4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repetition_task_ex1p4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 1;
+            int counter2 = 0;
+            string input = Console.ReadLine();
+            int inputparse = int.Parse(input);
+
+            while (counter > 0)
+            {
+                Console.Write("X");
+                counter = counter - 1;
+                if (counter == 0)
+                {
+                    counter2 = counter2 + 1;
+                    counter = 1 + counter2;
+                    Console.WriteLine();
+                    if (counter == inputparse + 1)
+                    {
+                        counter = 0;
+                    }
+                }
+            }
+
+            // TODO: Implement Functionality Here
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ repetition task ex2p3/repetition task ex2p3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repetition_task_ex2p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int counter = 0;
+            int space = 0;
+            int spacecounter = 0;
+            int xamount = 0;
+            int xamountcount = 0;
+
+            while (counter < 6)
+            {
+                counter = counter + 1;
+                space = 6 - counter;
+                if (counter == 1)
+                {
+                    xamount = counter;
+                    space = 6 - 1;
+                }
+                else
+                {
+                    xamount = ((counter + 1) * 2) - 3;
+                    space = 6 - counter;
+                }
+                while (spacecounter < space)
+                {
+                    spacecounter = spacecounter + 1;
+                    Console.Write(" ");
+                }
+                spacecounter = 0;
+                while (xamountcount < xamount)
+                {
+                    xamountcount = xamountcount + 1;
+                    Console.Write("x");
+                }
+                xamountcount = 0;
+                Console.WriteLine();
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
